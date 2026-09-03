ecce2a3dd550ada5c90db80d2f298941935d5020
ED1 <pedrohenriquebazilio8@gmail.com>
2026-09-02 21:45:29 -0300

    commit

--- src/Exer01_BubbleSortSwapCount.cpp
+++ src/Exer01_BubbleSortSwapCount.cpp
@@ -10,23 +10,7 @@ using namespace std;
 using ll = long long;
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
-class solve {
-    public:
-    bool is_sorted(const vector<int>& arr) {
-        ll swap_count = 0;
-        int n = static_cast<int>(arr.size());
 
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr[j], arr[j + 1]);
-                    swap_count++;
-                }
-            }
-        }
-        return true;
-    }
-};
 // TODO: Implement Bubble Sort in ascending order and return the total number of swaps performed
 ll bubble_sort_swap_count(vector<int>& arr) {
     ll swap_count = 0;
